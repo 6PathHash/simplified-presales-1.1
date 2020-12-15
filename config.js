7ad1ede035340cd6df6cc76ca328cf9d6a8dc3b9
Jikun Liao <liaojikundever@gmail.com>
2020-12-15 11:57:54 +1000

    bitpower-presale launch

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const { ether, BN } = require("@openzeppelin/test-helpers");
 let config = {};
 
 config.timer = {
-  startTime: 1605408240,
-  refundTime: 1605408240 + 86400,
-  endTime: 1605408240 + 86400 * 2,
-  softCap: ether("500")
+  startTime: 1607922000,
+  refundTime: 1607922000 + 86400,
+  endTime: 1607922000 + 86400 * 2,
+  softCap: ether("150")
 };
 
 config.redeemer = {
@@ -16,30 +16,28 @@ config.redeemer = {
 
 config.presale = {
   maxBuyPerAddress: ether("25"),
-  uniswapEthBP: 5000,
-  uniswapLidEthBP: 2000,
+  uniswapEthBP: 4250,
+  uniswapLidEthBP: 1750,
   uniswapTusdEthBP: 0,
-  lidEthBP: 500,
-  hardcap: ether("750"),
-  token: "",
+  lidEthBP: 800,
+  hardcap: ether("2500"),
+  token: "0x52d904eff2605463c2f0b338d34abc9b7c3e3b08",
   uniswapRouter: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
   lidFund: "0x2afE616c6E7999EF5a569dD72830023066c0875f",
   access: "0xfD8e59814D601219bddd53879ADa1Ff75fD316e2",
-  marketingFund: "",
-  teamFund: "",
-  projectFund: "",
+  marketingFund: "0x904aEfA7Ed357b7184b72fF3F492CED11abE639D",
+  teamFund: "0x169898d4bce7bEeED085027Dc305991d77b5769a",
+  projectFund: "0xc53219ae46Dd6b017F2a3834D31085F0f198d847",
   lidLiqLocker: "0x2afE616c6E7999EF5a569dD72830023066c0875f",
   tusdLiqLocker: "0x52083d6A1680d42D8Fd2cD2e2F9edD18858bAcD9",
-  projectLock: "",
-  teamLock: "",
   tokenPoolsBP: {
     marketing: 500,
-    team: 1461,
+    team: 2200,
     lidFee: 100,
-    project: 2910,
-    liquidity: 1449,
-    presale: 3000,
-    lidLiq: 580,
+    project: 0,
+    liquidity: 1912,
+    presale: 4500,
+    lidLiq: 788,
     tusdLiq: 0
   }
 };
